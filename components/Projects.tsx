f4ce15eb0fd31429989e273f198a7d0571c595cd
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-12-24 16:20:37 +0330

    Styled the date and layout in phone
    - made the date font smaller
    - remove h-screen in project page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,7 +13,7 @@ interface ProjectsProps {
 
 const Projects: FC<ProjectsProps> = ({ projectItems }) => {
   return (
-    <main className="max-w-2xl mx-auto w-full px-4 h-screen mt-5">
+    <main className="max-w-2xl mx-auto w-full px-4 mt-5">
       <h3
         className={cn(
           'font-bold text-2xl md:text-4xl tracking-tight',
